31620bc736d396721242cefc877f4dfc100aa230
damola <thedamolaoladipo@gmail.com>
2025-09-29 13:44:43 +0100

    chore: development build

---
+++ app/layout.tsx
@@ -0,0 +1,82 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+import { siteConfig } from "@/_data/site-config";
+import { Analytics } from "@vercel/analytics/next"
+import { SpeedInsights } from "@vercel/speed-insights/next"
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: siteConfig.name,
+  description:siteConfig.description,
+  metadataBase: new URL(siteConfig.getStartedUrl),
+  keywords: [
+    "theglobalma",
+    "Product Management",
+    "Software Engineering",
+    "Data Science",
+    "Open Source Contribution"
+  ],
+  authors: [
+    {
+      name: siteConfig.title,
+      url: siteConfig.url,
+    },
+  ],
+  creator: siteConfig.title,
+  openGraph: {
+    type: "website",
+    locale: "en_US",
+    url: siteConfig.getStartedUrl,
+    title: siteConfig.name,
+    description: siteConfig.description,
+    siteName: siteConfig.name,
+    images: [
+      {
+        url: `${siteConfig.url}/blocks/og-image.png`,
+        width: 1200,
+        height: 630,
+        alt: siteConfig.name,
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: siteConfig.name,
+    description: siteConfig.description,
+    images: [siteConfig.ogImage],
+    creator: siteConfig.links.creator,
+  },
+  icons: {
+    icon: `${siteConfig.url}/blocks/theglobalma.png`,
+    apple: "/apple-touch-icon.png",
+  },
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        
+        {children}
+        <Analytics/>
+        <SpeedInsights/>
+      </body>
+    </html>
+  );
+}
